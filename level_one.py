b6af8a7044a5869a123ba8fc6f6626ef6015d463
Alex Wright <awright11@berklee.edu>
2025-04-22 18:42:03 -0400

    Level 1 Complete

---
+++ level_one.py
@@ -0,0 +1,3 @@
+print("\"What do you call a dinosaur with no legs?\" I paused, and thought at my sisters question. \nAnswering, I said \"Girl I don't know.\"")
+
+print("Line 1 \n Line 2 \n  Line 3 \n Line 4 \nLine 5")
