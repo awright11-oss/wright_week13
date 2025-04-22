a0aba1521af274be3536a3fb1bbd35e7612e3b3a
Alex Wright <awright11@berklee.edu>
2025-04-22 19:10:54 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,21 @@
+mix_of_stuff = ["twine", 4, True, None]
+
+#syntax = start at 1, stop at 4, skip every 2. colons separate objects.
+#print(mix_of_stuff[1:4:2])
+
+
+#check all of these artists out.
+playlist_coding = ["Mint Condition - So Fine", 
+                   "Hummel - Septet in D Minor, Allegro",
+                   "Bach - Brandenburg Concerto No. II, Adago", 
+                   "Wada Kanako - Sunday Brunch", 
+                   "Wrapped in the Fog - Junko Ohashi",
+                   "Tina Turner - I Don't Wanna Fight", 
+                   "Earth Wind & Fire - After The Love Has Gone"]
+
+playlist_coding.append("XTC - All of A Sudden")
+
+#this one is better.
+playlist_coding.append("Rick Astley - Whenever You Need Somebody")
+
+print(playlist_coding)
